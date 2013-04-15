a317a9f374dcdea01b64a03abbe36dbc8ada62c4
Gregory Matthew James <mammenx@gmail.com>
2013-04-15 18:44:30 +0530

    Added templates for ovm_env & ovm_seq

---
+++ templates/syn_ovm_template_env.sv
@@ -0,0 +1,101 @@
+/*
+ --------------------------------------------------------------------------
+   Synesthesia - Copyright (C) 2012 Gregory Matthew James.
+
+   This file is part of Synesthesia.
+
+   Synesthesia is free; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Synesthesia is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+ -- Project Code      : synesthesia
+ -- Component Name    : <env_name>
+ -- Author            : 
+ -- Function          : 
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+
+ -- $Header$
+ 
+
+ -- $Log$
+
+ --------------------------------------------------------------------------
+*/
+
+
+`ifndef __<env_name>
+`define __<env_name>
+
+
+  class <env_name> extends ovm_env;
+
+    //Parameters
+
+
+    /*  Register with factory */
+    `ovm_component_utils(<env_name>)
+
+
+    //Declare agents, scoreboards
+
+
+    OVM_FILE  f;
+
+
+    /*  Constructor */
+    function new(string name  = "<env_name>", ovm_component parent = null);
+      super.new(name, parent);
+    endfunction: new
+
+
+    /*  Build */
+    function void build();
+      super.build();
+
+      f = $fopen({"./logs/",get_full_name(),".log"});
+
+      set_report_default_file(f);
+      set_report_severity_action(OVM_INFO,  OVM_DISPLAY | OVM_LOG);
+      set_report_severity_action(OVM_WARNING, OVM_DISPLAY | OVM_LOG);
+      set_report_severity_action(OVM_ERROR,  OVM_COUNT | OVM_DISPLAY | OVM_LOG);
+      set_report_severity_action(OVM_FATAL,  OVM_EXIT | OVM_DISPLAY | OVM_LOG);
+
+      ovm_report_info(get_name(),"Start of build ",OVM_LOW);
+
+
+      ovm_report_info(get_name(),"End of build ",OVM_LOW);
+    endfunction
+
+
+    /*  Connect */
+    function void connect();
+      super.connect();
+
+      ovm_report_info(get_name(),"START of connect ",OVM_LOW);
+
+
+      ovm_report_info(get_name(),"END of connect ",OVM_LOW);
+    endfunction
+
+
+
+  endclass  : <env_name>
+
+`endif
---
+++ templates/syn_ovm_template_seq.sv
@@ -0,0 +1,72 @@
+/*
+ --------------------------------------------------------------------------
+   Synesthesia - Copyright (C) 2012 Gregory Matthew James.
+
+   This file is part of Synesthesia.
+
+   Synesthesia is free; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Synesthesia is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+ -- Project Code      : synesthesia
+ -- Sequence Name     : <seq_name>
+ -- Author            : 
+ -- Function          :
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+
+ -- $Header$
+ 
+
+ -- $Log$
+
+ --------------------------------------------------------------------------
+*/
+
+`ifndef __<seq_name>
+`define __<seq_name>
+
+  class <seq_name>   #(
+                        type  PKT_TYPE  = ,
+                        type  SEQR_TYPE =
+                     ) extends ovm_sequence  #(PKT_TYPE);
+
+    /*  Adding the parameterized sequence to the registery  */
+    typedef <seq_name>#(PKT_TYPE) this_type;
+    typedef ovm_object_registry#(this_type)type_id;
+
+    /*  Linking with p_sequencer  */
+    `ovm_declare_p_sequencer(SEQR_TYPE)
+
+
+    /*  Constructor */
+    function new(string name  = "<seq_name>");
+      super.new(name);
+    endfunction
+
+    /*  Body of sequence  */
+    task  body();
+      p_sequencer.ovm_report_info(get_name(),"Start of <seq_name>",OVM_LOW);
+
+    endtask : body
+
+
+  endclass  : <seq_name>
+
+`endif
