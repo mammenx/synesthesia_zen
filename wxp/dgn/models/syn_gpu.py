12ec8e20d857963230dac5e0c4342e88c02bb4b7
mammenx <mammenx@gmail.com>
2013-05-05 11:51:53 +0530

    Added color model for ycbcr

--- wxp/dgn/models/syn_gpu.py
+++ wxp/dgn/models/syn_gpu.py
@@ -1,4 +1,7 @@
 from graphics import *
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 
 "	Global variables	"
@@ -109,19 +112,61 @@ def draw_line_anti(x0,y0,x1,y1,w):
 
 
 
+def	plot_ycbcr_cube():
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	
+	for y in range (0, 16):
+		for cb in range (0, 4):
+			for cr in range (0, 4):
+				y1 = 16 + (y*14.6)
+				cb1= 16 + (cb*74.66667)
+				cr1= 16 + (cr*74.66667)
+				
+				r = (1.164*(y1 - 16) + 1.793*(cr1 - 128))
+				g = (1.164*(y1 - 16) - 0.213*(cb1 - 128) - 0.533*(cr - 128))
+				b = (1.164*(y1 - 16) + 2.112*(cb1 - 128))
+				
+				if(r<0):
+					r = 0
+				elif(r > 255):
+					r = 255
+				
+				if(g<0):
+					g = 0
+				elif(g > 255):
+					g = 255
+				
+				if(b<0):
+					b = 0
+				elif(b > 255):
+					b = 255
+				
+				ax.scatter(y,cb,cr, c=color_rgb(r,g,b), marker='o', s=200)
+	
+	ax.set_xlabel('Y')
+	ax.set_ylabel('Cb')
+	ax.set_zlabel('Cr')
+	
+	plt.show()
+
+
 "	Work Area	-	Write code to test models here ..."
-win = GraphWin('syn_canvas ' + str(canvas_w) + 'x' + str(canvas_h),canvas_w,canvas_h)
-
-pt = Point(200,200)
-pt.draw(win)
-
-draw_line_anti(0,0,100,10,win)
-draw_line_anti(0,0,10,100,win)
+"win = GraphWin('syn_canvas ' + str(canvas_w) + 'x' + str(canvas_h),canvas_w,canvas_h)"
+""
+"pt = Point(200,200)"
+"pt.draw(win)"
+""
+"draw_line_anti(0,0,100,10,win)"
+"draw_line_anti(0,0,10,100,win)"
 " draw_line_anti(0,200,400,0,win) "
-draw_line_anti(400,0,0,200,win)
-draw_line_anti(0,0,10,canvas_h,win)
-draw_line_anti(0,0,canvas_w,20,win)
-draw_line_anti(0,0,400,400,win)
+"draw_line_anti(400,0,0,200,win)"
+"draw_line_anti(0,0,10,canvas_h,win)"
+"draw_line_anti(0,0,canvas_w,20,win)"
+"draw_line_anti(0,0,400,400,win)"
+
+
+plot_ycbcr_cube()
 
 
 input("waiting ...")
